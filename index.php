8194561e2da0150ca04e15eadadd3d176045d4cd
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-01-27 17:11:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-echo "Hello World";
+<?php 
+include 'config.php';
+include 'class.stocks.php';
+header('Content-Type: application/json');
+$stocks = new Stocks();
+$list=$stocks->get_stocks();
+echo "{\"stocks\":";
+echo json_encode($list);
+echo "}";
+?>
