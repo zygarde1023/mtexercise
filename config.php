60bc7d064a28e91fc92d6fa5308f7d9cf091ca0c
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-01-27 17:01:21 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-define('DB_SERVER','localhost');
-define('DB_USERNAME','root');
-define('DB_PASSWORD','');
-define('DB_DATABASE','db_store');
+define('DB_SERVER','https://www.obvibase.com/p/pEbZvkaRl9BWv61a');
+define('DB_USERNAME','zbcawalingusls1023@gmail.com');
+define('DB_PASSWORD','zhandro12');
+define('DB_DATABASE','db');
