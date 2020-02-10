11dd56db08a91282848a9fd071ebafc651b5b0a4
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-02-10 17:08:27 +0800

    Create class.cars.php

---
+++ class.cars.php
@@ -0,0 +1,22 @@
+<?php
+class Cars{
+	public $db;
+	
+	public function __construct(){
+		$this->db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+		if(mysqli_connect_errno()){
+			echo "Database connection error.";
+			exit;
+		}
+	}
+	
+	public function get_cars(){
+		$sql = "SELECT * FROM tbl_cars";
+		$result = mysqli_query($this->db,$sql);
+		while($row = mysqli_fetch_assoc($result)){
+			$list[] = $row;
+		}
+		return $list;
+	}
+	
+}
