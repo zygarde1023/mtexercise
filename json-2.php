95107acedfa8a85a58b125ba1faba62ebd4995f6
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-02-10 17:15:48 +0800

    Update json-2.php

--- json-2.php
+++ json-2.php
@@ -1,20 +1,70 @@
+<style>
+
+    .content{
+        display: flex;
+        justify-content: center;
+    }
+	table{
+		border: 1px solid black;
+		width: 100%;
+		text-align: center;
+		
+	}
+	th, td{
+		border: 1px solid #ddd;
+		padding: 10px;
+
+	}
+    tr:nth-child(even) {background-color: blue;}
+	a {
+		color: #ccf2ff;
+	}
+    .head{
+		background-color: #45a29e;
+		color: #1f2833;
+		font-weight: bold;
+	}
+</style>
+
+
 <?php
-$json = file_get_contents('http://localhost/json/json-1.php');
+$json = file_get_contents('https://new-app-json.herokuapp.com/json-1.php');
 
 $data = json_decode($json,true);
-$list = $data['stocks'];
-//$list = $data['stocks'][1];
-//$list = $data['stocks'][0]['sto_name'];
+$list = $data['cars'];
+//$list = $data['cars'][1];
+
 
 //var_dump($data);
 //echo "<pre>";
 
 //print_r($list);
-
+?>
+<div class="content">
+<h1>Cars</h1>
+</div>
+<div class="content">
+<table border="1px">
+    <tr class="head">
+        <td>Car ID</td>
+        <td>Car Brand</td>
+        <td>Car Status</td>
+        <td>Car Name</td>
+        <td>Car Price</td>
+    </tr>
+<?php
 foreach($list as $value){
-  echo $value['sto_id'];
-  echo $value['sto_name'];
-  echo $value['sto_description'];
-  echo $value['sto_price'];
+    ?>
+    <tr>
+        <td><?php echo $value['id'];?></td>
+        <td><?php echo $value['carbrand'];?></td>
+        <td><?php echo $value['carstatus'];?></td>
+        <td><?php echo $value['carname'];?></td>
+        <td><?php echo $value['carprice'];?></td>
+    </tr>
+ 
+<?php
 }
 ?>
+</table>
+</div>
