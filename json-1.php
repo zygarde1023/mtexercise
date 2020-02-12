4d767bb9364cc9bb94f07a6b2003ef93f28d5fa7
zygarde1023 <60255227+zygarde1023@users.noreply.github.com>
2020-02-12 14:51:08 +0800

    Update json-1.php

--- json-1.php
+++ json-1.php
@@ -3,7 +3,7 @@ include 'config.php';
 include 'class.cars.php';
 header('Content-Type: application/json');
 $cars = new Cars();
-$list=$pets->get_cars();
+$list=$cars->get_cars();
 echo "{\"cars\":";
 echo json_encode($list);
 echo "}";
